95c6e24cd04c80d92ece3be3dd165d01b59f0b74
sihyunjojo <si4018@naver.com>
2023-10-25 16:51:29 +0900

    자바 11 querydsl 5.0.0 springboot 2.7.8버전 환경에서 안정

--- src/main/java/project/studycafe/app/security/SecurityConfig.java
+++ src/main/java/project/studycafe/app/security/SecurityConfig.java
@@ -1,39 +1,74 @@
 package project.studycafe.app.security;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import project.studycafe.app.domain.enums.MemberLevel;
-import project.studycafe.app.domain.member.Member;
 import project.studycafe.app.service.oauth.OAuthService;
 
 
 // 어댑터는 호환되지 않는 두개의 인터페이스나 클래스를 연결하여 함께 작동하도록 만드는 역할을 한다.
 // 스프링 시큐리티를 사용하면 /oauth2/authorization/{OAuth 서비스 이름} 형식으로 요청만 보내면 알아서 다 해결해줍니다.
+
+@Configuration
 @EnableWebSecurity //spring security 설정을 활성화시켜주는 어노테이션
 @RequiredArgsConstructor //final 필드 생성자 만들어줌
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     private final OAuthService oAuthService;
 
+//    @Bean
+//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//        http
+//                .csrf().disable()
+//                .headers().frameOptions().disable();
+//
+//        http.authorizeHttpRequests((authz) -> {
+//                    try {
+//                        authz
+//                                .and()
+//                                    .exceptionHandling()
+//                                    .accessDeniedPage("/template/access-denied.html")
+//                                .and()
+//                                    .formLogin()
+//                                        .loginPage("/login")
+//                                        .loginProcessingUrl("/api/login")
+//                                        .defaultSuccessUrl("/oauth/success", true)
+//                                .and()
+//                                    .logout()
+//                                    .logoutSuccessUrl("/")
+//                                .and()
+//                                    .oauth2Login()
+//                                        .loginPage("/login")
+//                                        .defaultSuccessUrl("/oauth/success", true)
+//                                 .userInfoEndpoint()
+//                            .userService(oAuthService);
+//
+//                    }catch (Exception e){
+//                        throw new RuntimeException(e);
+//                    }
+//                });
+//
+//        return http.build();
+//    }
 
-    // OauthMember에 대해서만 적용이 되는거 같음. 그치그치 그게 맞는거 같아
+//     OauthMember에 대해서만 적용이 되는거 같음. 그치그치 그게 맞는거 같아
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-
         http
-                .csrf().disable()//csrf 공격을 막아주는 옵션을 disalbe, rest api같은 경우에는 브라우저를 통해 request 받지 않기 때문에 해당 옵션을 꺼도 됩니다.
+                .csrf().disable()   //csrf 공격을 막아주는 옵션을 disalbe, rest api같은 경우에는 브라우저를 통해 request 받지 않기 때문에 해당 옵션을 꺼도 됩니다.
                 .headers().frameOptions().disable()
                 .and()
                     .authorizeHttpRequests()
-                    .antMatchers("/", "/css/**", "/image/**", "/js/**").permitAll()
-                    .antMatchers("/seat/**").hasRole(MemberLevel.USER.name())
+                        .antMatchers("/", "/css/**", "/image/**", "/js/**").permitAll()
+                        .antMatchers("/seat/**").hasRole(MemberLevel.USER.name())
                 .and()
                     .exceptionHandling()
                     .accessDeniedPage("/template/access-denied.html") // 접근 거부 페이지 설정 status가 403일떄,
                 .and()
                     .logout()   //기본 logout url = "/logout"
-                     .logoutSuccessUrl("/") //logout 요청시 홈으로 이동
+                    .logoutSuccessUrl("/") //logout 요청시 홈으로 이동
                 .and()
                     .oauth2Login() //OAuth2 로그인 설정 시작점
                         .loginPage("/login")// oauth를 로그인하기 위해서 가야하는 주소폼을 입력해주는거임.
@@ -42,10 +77,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
                 // 이 코드를 작성하니까 기존 @{/login}을 사용했을때 ouath 이상한 화면으로 넘어가던게 그냥 됬음.
                 //// 로그인이 되어 있어도 Oauth으로 로그인이 안되어 있으니까 이 방법으로 로그인하라고 login으로 넘겨줌.
-                //
                         .defaultSuccessUrl("/oauth/success", true) //OAuth2 성공시 redirect
                         .userInfoEndpoint() //OAuth2 로그인 성공 이후 사용자 정보를 가져올 때 설정 담당
                             .userService(oAuthService); //OAuth2 로그인 성공 시, 작업을 진행할 oAuthService
-
     }
 }
